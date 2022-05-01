5040b52a3f4c032abaf86af8326d22f31a6a4b7a
HALRINSKEY <hal4554yoko@gmail.com>
2022-05-01 16:00:00 +0900

    Update time.py

--- time.py
+++ time.py
@@ -22,10 +22,10 @@ async def on_voice_state_update(member, before, after):
     # チャンネルへの入室ステータスが変更されたとき（ミュートON、OFFに反応しないように分岐）
     if before.channel != after.channel:
         # 通知メッセージを書き込むテキストチャンネル（チャンネルIDを指定）
-        botRoom = client.get_channel(966831929058025482)
+        botRoom = client.get_channel(949091439969697814)
  
         # 入退室を監視する対象のボイスチャンネル（チャンネルIDを指定）
-        announceChannelIds = [966831929058025483]
+        announceChannelIds = [842449150321885188, 946120184333221958, 946120235193356308]
  
         # 入室通知
         if after.channel is not None and after.channel.id in announceChannelIds:
